786032f2faf32ce82add3b40c7fd6bf2596e214c
Basheir <php0@hotmail.com>
2017-12-13 09:57:35 +0300

    Add files via upload

---
+++ pushTelegram.3/admin/language/ar/extension/module/pushtelegram.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Basheir
+ * 1-11-2017
+ *  Push Telegram
+ */
+$_['heading_title'] = 'Push Telegram';
+$_['entry_description'] = 'ضبط الاعدادات لارسال التنبيهات';
+$_['entry_app_key'] = 'Application BOT_TOKEN';
+$_['entry_user_key'] = 'PushTelegram Chat ID';
+$_['entry_send_order_alert'] = 'ارسال تنبيه عند طلب جديد';
+$_['error_no_key'] = 'لم تقم بادخال - BOT_TOKEN';
+$_['text_module']      = 'Modules';
+$_['entry_add_amount_total']      = 'اضافة مجموع النتجات';
+$_['entry_add_products']      = 'اضافة المنتجات اللي تم شرائها';
+$_['message_sent_success'] = 'تم ارساله التنبيه بنجاح';
+$_['message_sent_error'] = 'لايمكن ارسال اتنبيه - الرجاء التحقق من الاعدادات';
+$_['text_edit'] = 'تعديل تنبيهات / Push Telegram';
+$_['header_customer_message'] = ' اعدادات المستخدم';
+$_['entry_send_new_customer_alert'] = 'تنبيه عند تسجيل مستخدم جديد';
+$_['entry_add_customer_name'] = 'اضافة اسم المستخدم للتنبيه';
+
+?>
---
+++ pushTelegram.3/admin/language/en-gb/extension/module/pushtelegram.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * TomIT
+ * 29-11-2013
+ * Translation file for pushtelegram
+ */
+$_['heading_title'] = 'Push Messages';
+$_['entry_description'] = 'In order to use push messages you need a <a href="http://www.pushtelegram.net" target="_blank">Pushover Account</a>';
+$_['entry_app_key'] = 'Application API Key';
+$_['entry_user_key'] = 'Pushover User Key';
+$_['entry_send_order_alert'] = 'Send alert on new order';
+$_['entry_add_amount_total'] = 'Add total order amount to message';
+$_['entry_add_products'] = 'Add ordered products to message';
+$_['entry_add_customer_name'] = 'Add new customer name to message';
+$_['entry_send_new_customer_alert'] = 'Send alert on new customer';
+$_['header_order_message'] = '<h2>New Order settings</h2>';
+$_['header_customer_message'] = '<h2>New Customer settings</h2>';
+$_['error_no_key'] = 'No User Key or Api Key entered';
+$_['text_module']      = 'Modules';
+$_['message_sent_success'] = 'Message sent succesfully';
+$_['message_sent_error'] = 'Message couldnt be sent, please check your details';
+$_['text_edit'] = 'Edit Push Notification Settings';
+?>
